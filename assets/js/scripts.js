bc0fcee934b3d39970b3d372ab70545b08742279
Daniel Lochner <daniel@daniellochner.com>
2025-04-08 20:53:32 +0200

    Add website files

---
+++ assets/js/scripts.js
@@ -0,0 +1,26 @@
+function setDialogVisibility(dialogId, isVisible) {
+    document.getElementById(dialogId).style.display = isVisible ? "flex" : "none";
+
+    if (!isVisible) {
+        window.location.hash = "#";
+    }
+}
+
+function joinDiscord() {
+    window.open("https://discord.com/invite/freelives");
+}
+
+function showPressKit() {
+    window.open("https://drive.google.com/drive/u/0/folders/1iwVFek-EFrIq_keNM2JSQ9hIecBhiXI_");
+}
+
+let hash = window.location.hash;
+switch (hash) {
+    case "#privacy-policy":
+        setDialogVisibility("privacy-policy", true);
+        break;
+    case "#contact":
+    case "#support":
+        setDialogVisibility("support", true);
+        break;
+}
